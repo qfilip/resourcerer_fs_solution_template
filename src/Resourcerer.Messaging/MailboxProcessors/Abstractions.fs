644af0498069fa192363b313392328bbc33d1e24
qfilip <filip.kukolj@arcadis.com>
2025-10-02 16:53:53 +0200

    added messaging project

---
+++ src/Resourcerer.Messaging/MailboxProcessors/Abstractions.fs
@@ -0,0 +1,7 @@
+﻿namespace Resourcerer.Messaging.MailboxProcessors.Abstractions
+
+type IAsyncVoidProcessor<'a> =
+    abstract member Post: 'a -> unit
+
+type IAsyncReplyProcessor<'a, 'b> =
+    abstract member Post: 'a -> Async<'b>
---
+++ src/Resourcerer.Messaging/MailboxProcessors/Types.fs
@@ -0,0 +1,35 @@
+﻿namespace Resourcerer.Messaging.MailboxProcessors.Types
+
+open Resourcerer.Messaging.MailboxProcessors.Abstractions
+
+type ReplyProcessorMessage<'a, 'b> = 'a * AsyncReplyChannel<'b>
+
+type AsyncVoidProcessor<'a>(asyncHandler: 'a -> Async<unit>) =
+    let agent = MailboxProcessor<'a>.Start(fun mailbox ->
+        let rec loop () = async {
+            let! message = mailbox.Receive()
+            do! asyncHandler message
+            
+            do! loop ()
+        }
+
+        loop ()
+    )
+    interface IAsyncVoidProcessor<'a> with
+        member _.Post(x) = agent.Post x
+
+type AsyncReplyProcessor<'a, 'b>(asyncHandler: 'a -> Async<'b>) =
+    let agent = MailboxProcessor<ReplyProcessorMessage<'a, 'b>>.Start(fun mailbox ->
+        let rec loop () = async {
+            let! message, rc = mailbox.Receive()
+            let! reply = asyncHandler message
+            rc.Reply(reply)
+            
+            do! loop ()
+        }
+
+        loop ()
+    )
+    interface IAsyncReplyProcessor<'a, 'b> with
+        member _.Post(x) = agent.PostAndAsyncReply(fun rc -> x, rc)
+
